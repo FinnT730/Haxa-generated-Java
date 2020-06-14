e43445710c9b6b25c408947b3de9027ae5aa9a3f
FinnT730 <finnt730@gmail.com>
2020-06-14 15:00:00 +0200

    Add files via upload
    ...;;;

---
+++ src/Main.hx
@@ -0,0 +1,17 @@
+class Main {
+
+    public static function main() {
+        for(i in [1,2,3,4,5,6,7,8,9]) {
+            trace(i);
+        }
+
+
+    }
+
+
+    public function new () {
+
+
+    }
+
+}
